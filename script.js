6e847c2abbdd4542bee65edc7b0ab4764d4d5a57
Mariam Saeed <mariamsaeed750@gmail.com>
2023-05-29 21:12:53 +0300

    added copy to clipboard feature

---
+++ script.js
@@ -0,0 +1,29 @@
+const codeSnippets = document.querySelectorAll('pre');
+
+//* create element for icon
+const copyIcon = document.createElement('img');
+copyIcon.src = './copy.png';
+copyIcon.alt = 'copy-img';
+copyIcon.className = 'icon';
+
+// add icons to the code snippets
+codeSnippets.forEach((code) => {
+  code.prepend(copyIcon.cloneNode(true));
+});
+
+const icons = document.querySelectorAll('.icon');
+
+const handleClick = (e) => {
+  if (e.target.alt === 'copy-img') {
+    const code = e.currentTarget.querySelector('code');
+    copyText(code);
+  }
+};
+
+const copyText = (code) => {
+  navigator.clipboard.writeText(code.textContent);
+};
+
+codeSnippets.forEach((code) => {
+  code.addEventListener('click', handleClick);
+});
